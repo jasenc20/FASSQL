cb31fa9f2fb0b9fd984efcadfb735e7011466115
jasenc20 <jasenclerisier@gmail.com>
2026-08-02 22:06:50 -0400

    Leverage threading to allow for multiple connection to FASSQL

--- SERVER/POSTMASTER/postmaster.py
+++ SERVER/POSTMASTER/postmaster.py
@@ -2,21 +2,40 @@
 import socket
 import sys
 from CLI_USER_INPUT.userInput import cli
+import threading 
 
 
 
 HOST = ''
 PORT = 8020
 
+def handle_client(conn, address):
+    print('Connected with ' + address[0] + ':' + str(address[1]))
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                print('Client disconnected')
+                break
+
+            message = data.decode().strip()
+            print(f'Received: {message}')
+
+            if message.lower() == 'quit':
+                break
+
+            conn.sendall(message.encode())
+
+
+
 def create_server():
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-            soc.bind((HOST,PORT))
+        soc.bind((HOST,PORT))
     except socket.error as message:
-            print('Bind failed. Error Code: ' 
-                + str(message[0])+ ' Message' + message[1])
-            sys.exit()
+        print(f'Bind failed. Error Code: {message.errno} Message: {message.strerror}')
+        sys.exit()
 
     print('Socket binding operation completed')
 
@@ -27,20 +46,8 @@ def create_server():
     print('Connected with '+ address[0] + 
             ':' + str(address[1]))
     
+
     while True:
-        data = conn.recv(1024) #READ up to 1024 bytes from the client
-        if not data:
-            print('Client disconnected')
-            break
-
-        message = data.decode().strip()
-        print(f'Received: {message}')
-
-        if message.lower() == 'quit':
-            break
-
-
-        conn.sendall(message.encode())
-
-    conn.close()
-    soc.close()
+        conn,address = soc.accept()
+        t = threading.Thread(target=handle_client, args=(conn, address))
+        t.start()
